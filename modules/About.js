33f6f3b4eaaa1bd097ab630a7918c004f85b1c0d
Teodor Rupi <teodorrupi@gmail.com>
2017-02-13 00:02:59 +0100

    clean react plus react-reducer project

---
+++ modules/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is the content of about page in a paragraph tag. This section should be visible below the common navigation system.</p>
+      </div>
+      )
+  }
+})
